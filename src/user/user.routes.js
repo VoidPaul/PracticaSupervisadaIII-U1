37b353e225127d963ce530fa98e78f3600a79133
Paul Ramos <palvarez-2023238@kinal.edu.gt>
2025-02-19 17:29:54 -0600

    added: routes for updating profile pictures.

--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import { getUserById, updateUser, updatePassword } from "./user.controller.js"
+import { getUserById, updateUser, updatePassword, updateProfilePicture } from "./user.controller.js"
 import {
   getUserByIdValidator,
   updateUserValidator,
   updatePasswordValidator,
+  //updateProfilePictureValidator
 } from "../middleware/user-validator.js"
 
 const router = Router()
@@ -13,5 +14,11 @@ router.get("/:uid", getUserByIdValidator, getUserById)
 router.put("/update/:uid", updateUserValidator, updateUser)
 
 router.patch("/update/password/:uid", updatePasswordValidator, updatePassword)
+
+router.patch(
+  "/picture/:uid",
+  updateProfilePicture.single("profilePicture")
+  /**updateProfilePictureValidator, updateProfilePicture */
+)
 
 export default router
