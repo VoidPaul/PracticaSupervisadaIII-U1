fc9d0874199adc6ca4db95bf788932070623ebe8
Paul Ramos <palvarez-2023238@kinal.edu.gt>
2025-02-19 17:25:05 -0600

    added: controller logic for profile picture edition.

--- src/user/user.controller.js
+++ src/user/user.controller.js
@@ -1,5 +1,11 @@
 import { hash, verify } from "argon2"
 import User from "./user.model.js"
+import fs from "fs/promises"
+import { join, dirname } from "path"
+import { fileURLToPath } from "url"
+import { unlink } from "fs"
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export const getUserById = async (req, res) => {
   try {
@@ -76,6 +82,49 @@ export const updatePassword = async (req, res) => {
     return res.status(500).json({
       success: false,
       message: "Error changing password.",
+      error: err.message,
+    })
+  }
+}
+
+export const updateProfilePicture = async (req, res) => {
+  try {
+    const { uid } = req.params
+
+    let newProfilePic = req.file ? req.file.filename : "default-pfp.png"
+
+    if (!newProfilePic) {
+      return res.status(400).json({
+        success: false,
+        message: "File not found.",
+      })
+    }
+
+    const user = await User.findById(uid)
+
+    if (user.profilePicture) {
+      const oldProfilePic = join(
+        __dirname,
+        "../../public/uploads/pictures/profile",
+        user.profilePicture
+      )
+
+      fs.unlink(oldProfilePic)
+    }
+
+    user.profilePicture = newProfilePic
+
+    await user.save()
+
+    return res.status(200).json({
+      success: true,
+      message: "Picture updated.",
+      profilePicture: user.profilePicture,
+    })
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Error updating profile picture.",
       error: err.message,
     })
   }
