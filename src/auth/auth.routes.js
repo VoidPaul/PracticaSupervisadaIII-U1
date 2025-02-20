ae8c6c200a03a74c863b2c3eb46fb295fdba907a
Paul Alvarez <palvarez-2023238@kinal.edu.gt>
2025-02-20 09:56:01 -0600

    refactorized: user-validator name to improve consistency with future modules.

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express"
 import { register, login } from "./auth.controller.js"
-import { registerValidator, loginValidator } from "../middleware/user-validator.js"
+import { registerValidator, loginValidator } from "../middleware/validate-user.js"
 import { uploadProfilePicture } from "../middleware/multer-uploads.js"
 
 const router = Router()
